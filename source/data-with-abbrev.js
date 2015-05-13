cbdea4e4217eb2d6faf28a3b04cc8d5174a35fdb
Sven Pedersen <sven.pedersen@crossroadsmusic.com>
2015-05-13 10:04:04 -0400

    add list of all codes TODO
    add list of country codes with completed countries removed (this
    comment should be removed entirely when this file is completed)

--- source/data-with-abbrev.js
+++ source/data-with-abbrev.js
@@ -3,6 +3,17 @@
 
 // As an example of how it works, see the Canada country data set
 
+// Regions that need ISO3166-2 codes (please update when you add):
+// http://en.wikipedia.org/wiki/ISO_3166-2
+// AD AE AG AI AM AO AQ AR AS AT AW AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BS BT
+// BV BW BY BZ CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG
+// ER ES ET FI FJ FK FM FO FR GA GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT
+// HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS
+// LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI
+// NL NP NR NU OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU SA SB SC SD SE SG
+// SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA
+// UM US UY UZ VA VC VE VU YT
+
 // first line must be comment
 var _data = [
   ["Afghanistan","AF","Badakhshan~BDS|Badghis~BDG|Baghlan~BGL|Balkh~BAL|Bamyan~BAM|Daykundi~DAY|Farah~FRA|Faryab~FYB|Ghazni~GHA|Ghor~GHO|Helmand~HEL|Herat~HER|Jowzjan~JOW|Kabul~KAB|Kandahar~KAN|Kapisa~KAP|Khost~KHO|Kunar~KNR|Kunduz~KDZ|Laghman~LAG|Logar~LOW|Maidan Wardak~WAR|Nangarhar~NAN|Nimruz~NIM|Nuristan~NUR|Paktia~PIA|Paktika~PKA|Panjshir~PAN|Parwan~PAR|Samangan~SAM|Sar-e Pol~SAR|Takhar~TAK|Urozgan~ORU|Zabul~ZAB"],
